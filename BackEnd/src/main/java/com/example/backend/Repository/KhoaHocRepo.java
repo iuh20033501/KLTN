0d74fd43807ecf40f7e5dbd4b6141953d8ac5d30
Huy Hung <hungro8102@gmail.com>
2024-10-03 14:36:34 +0700

    xoa backend

--- BackEnd/src/main/java/com/example/backend/Repository/KhoaHocRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.Repository;
-
-import com.example.backend.moudel.KhoaHoc;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KhoaHocRepo extends JpaRepository<KhoaHoc,Long> {
-}
