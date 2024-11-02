fd9aa95d218b78dd78607ea21a55414a9be5914c
KhaiKhua <quangkhai24022002@gmail.com>
2024-11-02 16:36:17 +0700

    fixx ThanhToan

--- BackEnd/src/main/java/fit/iuh/backend/repository/ThanhToanRepo.java
+++ BackEnd/src/main/java/fit/iuh/backend/repository/ThanhToanRepo.java
@@ -23,7 +23,10 @@ public interface ThanhToanRepo extends JpaRepository<ThanhToan, Long> {
     @Query("SELECT tt FROM ThanhToan tt JOIN tt.hoaDon hd ON tt.hoaDon.idHoaDon = hd.idHoaDon WHERE tt.hoaDon.idHoaDon = :id")
     List<ThanhToan> findByIdHD(@Param("id") Long idHD);
     @Query("SELECT tt FROM ThanhToan tt JOIN tt.nguoiThanhToan hv ON tt.nguoiThanhToan.idUser = hv.idUser WHERE tt.nguoiThanhToan.idUser = :id AND tt.trangThai = :trangThai")
-    List<ThanhToan> findByIdHVAndTrangThaiWait(@Param("id") Long idHV, @Param("trangThai") ThanhToanEnum trangThai);
+    List<ThanhToan> findByIdHVAndTrangThai(@Param("id") Long idHV, @Param("trangThai") ThanhToanEnum trangThai);
+
+    @Query("SELECT tt FROM ThanhToan tt JOIN tt.lopHoc lop ON tt.lopHoc.idLopHoc = lop.idLopHoc WHERE tt.lopHoc.idLopHoc = :id AND tt.trangThai = :trangThai")
+    List<ThanhToan> findByIdLopAndTrangThai(@Param("id") Long idLop, @Param("trangThai") ThanhToanEnum trangThai);
 
 }
 
